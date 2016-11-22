ade1fd9dd67a6e36f0715bc595f41848311d6fb4
roelien <roelientippe@gmail.com>
2016-11-22 12:46:41 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -17,63 +17,23 @@ app.get('/', function(req, res){
 console.log('listening on *:'+ port);
 
 
-var ballArr=[
-    {
-    x:100,
-    y:100,
-    r: 10,
-    stepX:5,
-    stepY:5,  
-    },
-];
 
-
-var platformArr=[
-    {
-        platformX:50,
-        platformY:50,
-        width: 20,
-        height: 100,    
-    },
-    {
-        platformX: 250,
-        platformY: 50,
-        width: 20,
-        height: 100,
-    }
-
-];
-
-
-//function moveBall(ball){
-//    ball.x += ball.stepX;
-//    ball.y += ball.stepY;
-//  
-//    if(ball.x == 470 || ball.x == 30){
-//       ball.stepX = -ball.stepX;
+setInterval( function() {
+    
+//    for(var i=0;i<platformArr.length; i++) {
+//        var thisPlatform=platformArr[i];
+//
+////        moveBall(thisBall);
 //    }
-//    if (ball.y == 30 || ball.y == 270){
-//       ball.stepY = -ball.stepY;
-//    }    
-//    return ball;
-//}
-io.sockets.on("connection", function(socket){
+//    
+//    io.sockets.emit('canvas', platformArr);
+//    console.log (thisPlatform);
+//    
+    io.sockets.on("connection", function(socket){
     socket.on('orientation', function(data){
         console.log('orientation changed', data);
         //paddle bewegen op de goede 
     });
 });
-
-setInterval( function() {
-    
-    for(var i=0;i<platformArr.length; i++) {
-        var thisPlatform=platformArr[i];
-
-//        moveBall(thisBall);
-    }
-    
-    io.sockets.emit('canvas', platformArr);
-    console.log (thisPlatform);
-    
     
 }, 1000);
